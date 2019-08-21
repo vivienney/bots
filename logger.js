510d9594e4670993ec35045c458f5919202c5413
TroxepYT <savkoviczarko95@gmail.com>
2019-08-21 15:48:57 +0300

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,48 @@
+require("colour");
+
+const Logger = {
+
+    spaces: '     ',
+    prefix: {
+        g: 'INFO: '.green,
+        a: 'ALERT:'.yellow,
+        e: 'ERROR:'.red
+    },
+
+    __: function(prefix, msg) {
+
+        console.log(Logger.spaces + " " + prefix + " " + msg.white.bold);
+    },
+
+    error: function() {
+        let msg = "";
+
+        for (let i in arguments) {
+            msg += arguments[i] + " ";
+        }
+
+        Logger.__(Logger.prefix.e, msg);
+    },
+
+    warn: function() {
+        let msg = "";
+
+        for (let i in arguments) {
+            msg += arguments[i] + " ";
+        }
+
+        Logger.__(Logger.prefix.a, msg);
+    },
+
+    good: function() {
+        let msg = "";
+
+        for (let i in arguments) {
+            msg += arguments[i] + " ";
+        }
+
+        Logger.__(Logger.prefix.g, msg);
+    }
+
+};
+module.exports = Logger;
